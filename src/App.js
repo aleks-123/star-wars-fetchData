d6a7863b9c4eba59d8b8bc6d6a57b24c5f4746dd
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2022-12-13 00:48:21 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // function fetchMoviesHandler() {
+  //   fetch("https://swapi.py4e.com/api/films/")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           realiseDate: movieData.release_date,
+  //         };
+  //       });
+  //       setMovies(transformedMovies);
+  //     });
+  // }
+
+  const fetchMoviesHandler = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch("https://swapi.py4e.com/api/films/");
+      if (!response.ok) {
+        throw new Error("something went wrong!!!");
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          realiseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading ....</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
